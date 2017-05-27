3fa2b78f5a2a4596ae631b3a103866a0aa380554
CaioQuiza <cai0.3duardo@gmail.com>
2017-05-26 21:04:37 -0300

    Add files via upload

---
+++ HUD.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+class HUD : MonoBehaviour
+{
+    public Texture2D heart;
+    public Texture2D lifeBar;
+    private int maxHP = 300;
+    public int hp;
+    public int numberOfKeys = 0;
+
+    public void Start()
+    {
+        hp = maxHP;
+    }
+
+    public void OnGUI()
+    {
+        for (int i = 0; i < numberOfKeys; i++)
+        {
+            GUI.DrawTexture(new Rect(10 + i * (heart.width + 10), 10, heart.width, heart.height), heart);
+        }
+
+        if (hp > 0)
+        {
+            GUI.BeginGroup(new Rect(10, Screen.height * 0.8F, hp, 50));
+            GUI.DrawTexture(new Rect(0, 0, maxHP, 50), lifeBar);
+            GUI.EndGroup();
+        }
+    }
+
+
+
+}
---
+++ LandmineControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandmineControl : MonoBehaviour {
+	public AudioSource Explosion;
+	public AudioListener Camera;
+	public GameObject Player;
+	public Transform explosionLocation;
+	public GameObject explosionPrefab;
+	
+	public void OnTriggerEnter (Collider hit) {
+
+		if (hit.gameObject.tag == "Player") {
+				
+			//GameObject a = GameObject.Instantiate (explosionPrefab, explosionLocation, 
+			//                        Quaternion.LookRotation(hit.rigidbody))as GameObject;
+			gameObject.GetComponent<PlayerCollision>().PlayerHealth -= 25;
+			Explosion.Play ();
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
